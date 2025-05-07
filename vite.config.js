68775a5dc82e587cd62a698956d1bc1813f1b8ed
Manoj Baskar <147849968+manojaba@users.noreply.github.com>
2025-05-07 14:27:29 +0530

    trial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+
+
+  base:"/quote-generator",
+  plugins: [tailwindcss(),react()]
 })
